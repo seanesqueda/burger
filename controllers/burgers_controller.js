87908329d9a526d448501895975b7fbe713be506
Sean Esqueda <sesqueda99@aol.com>
2019-08-30 04:52:16 -0700

    Get submit button working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,9 +15,9 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
     burger.insertOne([
-        "name", "devoured"
+        "burger_name", "devoured"
     ], [
-            req.body.name, req.body.devoured
+            req.body.burger_name, req.body.devoured
         ], function (result) {
             // Send back the ID of the new hamburger
             res.json({ id: result.insertId });
