a86e61c33e70225b4fea06641e34a203a22962b8
Sean Esqueda <sesqueda99@aol.com>
2019-09-08 07:20:46 -0700

    Finish heroku setup

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   var connection = mysql.createConnection({
-      host: "op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      host: "localhost",
       port: 3306,
-      user: "tv5qwsj3urpfj67l",
-      password: "	pj7q8agijq38ewew",
-      database: "wy2azn99ptgxwhi9"
+      user: "root",
+      password: "root",
+      database: "burgers_db"
   });
 }
 
